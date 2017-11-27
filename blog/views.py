80e681ad64a6f41092db1be7224523982078888f
GruzinskiyAl <gruzinskiy.al@gmail.com>
2017-11-27 13:28:04 +0200

    Post_detail for every post template added. View for post_detail added.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from .models import Post
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.order_by('id').reverse()
     return render(request, 'index.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html',  {'post': post})
